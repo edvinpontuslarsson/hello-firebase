1f3bb208c1c7641af348b8e30726f32ef4bb8ddf
Edvin Larsson <el223na@student.lnu.se>
2019-11-11 15:42:46 +0100

    adds prettier config (#4)

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+/**
+ * To format with prettier:
+ * Ctrl+Shift+i
+ */
+module.exports = {
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  printWidth: 60,
+};

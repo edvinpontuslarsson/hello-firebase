ac60bb30262f94e4dbf8dc2a70917dd9d8f4bc37
edvinpontuslarsson <el223na@student.lnu.se>
2019-11-11 20:45:36 +0100

    broken, has todo

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -5,6 +5,11 @@ import {
   Switch
 } from 'react-router-dom';
 
+// TODO:
+// perhaps just implement by
+// doing something like this in app:
+// https://github.com/armand1m/react-firebase-authentication-medium/blob/master/src/App.js
+
 const AppRouter: React.FC = (props) => {};
 
 export default AppRouter;

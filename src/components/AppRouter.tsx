c2bcfccfabf72e8720a5803b7e646269c5fcc860
edvinpontuslarsson <el223na@student.lnu.se>
2019-11-12 07:20:51 +0100

    b4 trying auth-library

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -3,12 +3,17 @@ import {
   BrowserRouter as Router,
   Route,
   Switch
-} from 'react-router-dom';
+} from 'react-router-dom'; // todo perhaps install typed one
+
+// or just delete this file
 
 // TODO:
 // perhaps just implement by
 // doing something like this in app:
 // https://github.com/armand1m/react-firebase-authentication-medium/blob/master/src/App.js
+// https://medium.com/firebase-developers/how-to-setup-firebase-authentication-with-react-in-5-minutes-maybe-10-bb8bb53e8834
+
+// render correct component based on logged in or not
 
 const AppRouter: React.FC = (props) => {};
 

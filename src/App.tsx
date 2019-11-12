4d86923c0da6edf8b86dd460254761b70aaee5ee
edvinpontuslarsson <el223na@student.lnu.se>
2019-11-12 07:52:53 +0100

    broken w. todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
+import firebase from 'firebase/app';
+import withFirebaseAuth from 'react-with-firebase-auth';
 import SignIn from './components/SignIn';
 import Hello from './components/Hello';
 import Initialize from './helpers/Initialize';
 
-const App: React.FC = () => {
+const App: React.FC = (props: any) => {
   Initialize.initialize();
-}; //
 
-export default App;
+  TODO: add more sign in options
+  const { user, signOut, signInWithGoogle } = props;
+
+  TODO: have app bar here, w sign out if in
+  // send in sign in stuff as props to Sign in
+  return <>{user ? <Hello /> : <SignIn />}</>;
+};
+
+const firebaseAppAuth = firebase.auth();
+
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider()
+};
+
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth
+})(App);

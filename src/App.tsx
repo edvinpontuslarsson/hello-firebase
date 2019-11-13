14bbee563a1fe92516e60ebcaf4e5f9ee3aae087
edvinpontuslarsson <el223na@student.lnu.se>
2019-11-13 19:07:28 +0100

    loading txt looks ok

--- src/App.tsx
+++ src/App.tsx
@@ -7,25 +7,44 @@ import Initialize from './helpers/Initialize';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import Typography from '@material-ui/core/Typography';
 
 const App: React.FC = () => {
-  const [user] = useAuthState(firebase.auth());
+  const [user, loading] = useAuthState(firebase.auth());
 
   return (
     <>
-      {user ? (
+      <div
+        style={{ textAlign: 'center', marginTop: '33px' }}
+      >
+        <Typography variant="h5">
+          Loading, please wait...
+        </Typography>
+      </div>
+    </>
+  );
+};
+
+/**
+ * {user ? (
         <main>
           <AppBar user={user} />
           <Reverse user={user} />
         </main>
+      ) : loading ? (
+        <div
+          style={{ textAlign: 'center', marginTop: '33px' }}
+        >
+          <Typography variant="h5">
+            Loading, please wait...
+          </Typography>
+        </div>
       ) : (
         <main>
           <SignIn />
         </main>
       )}
-    </>
-  );
-};
+ */
 
 Initialize.initialize();
 

d0d3e680045ae2e5e6dd69dd52940b0983301347
edvinpontuslarsson <el223na@student.lnu.se>
2019-11-13 13:03:30 +0100

    adds esrever lib

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,7 +4,17 @@ import * as admin from 'firebase-admin';
 admin.initializeApp();
 
 export const onTextCreate = functions.database
-  .ref('users/{userId}/text')
+  .ref('users/{userId}/texts/{textId}')
   .onCreate((snapshot, context) => {
     const textData = snapshot.val();
+    const reversedText: string = reverseString(
+      textData.content
+    );
   });
+
+function reverseString(str: string): string {
+  return str
+    .split('')
+    .reverse()
+    .join('');
+}

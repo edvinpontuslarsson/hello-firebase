26bbce644f0583f2abc3a2e46f78b29c4dd14c1c
edvinpontuslarsson <el223na@student.lnu.se>
2019-11-13 15:29:19 +0100

    gets contents in good way

--- src/components/Reverse.tsx
+++ src/components/Reverse.tsx
@@ -1,7 +1,8 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import ReverseForm from './ReverseForm';
 import firebase from 'firebase/app';
 import 'firebase/database';
+import { useListVals } from 'react-firebase-hooks/database';
 
 type ReverseProps = {
   user: firebase.User;
@@ -10,6 +11,19 @@ type ReverseProps = {
 const Reverse: FunctionComponent<ReverseProps> = ({
   user
 }) => {
+  const dbTextsPath = `users/${user.uid}/texts`;
+  const dbRef = firebase.database().ref(dbTextsPath);
+
+  /*
+  dbRef.on('value', (snapshot) => {
+    const textData = snapshot!.val();
+    // console.log(Object.values(textData));
+    setTexts(Object.values(textData));
+  });
+  */
+
+  const [stuff] = useListVals(dbRef);
+
   return (
     <>
       <div
@@ -17,12 +31,10 @@ const Reverse: FunctionComponent<ReverseProps> = ({
       >
         <ReverseForm
           onSubmit={({ text }) => {
-            firebase
-              .database()
-              .ref(`users/${user.uid}/texts`)
-              .push({ content: text });
+            dbRef.push({ content: text });
           }}
         />
+        {console.log(stuff)}
       </div>
     </>
   );

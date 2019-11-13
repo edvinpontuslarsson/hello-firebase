b9c7d7be51bd6654cbd798b9f37ddefd70ca5f33
Edvin Larsson <el223na@student.lnu.se>
2019-11-13 13:38:36 +0100

    Stored text gets reversed by cloud function (#9)
    * changes db structure
    
    * adds esrever lib
    
    * I am skipping esrever
    
    * reverses in correct manner
    
    * cloud funcs reverse texts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,18 @@
-// import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+export const onTextCreate = functions.database
+  .ref('users/{userId}/texts/{textId}')
+  .onCreate((snapshot) => {
+    const textData = snapshot.val();
+    const reversedText = reverseString(textData.content);
+    return snapshot.ref.update({ content: reversedText });
+  });
+
+function reverseString(str: string): string {
+  return Array.from(str)
+    .reverse()
+    .join('');
+}

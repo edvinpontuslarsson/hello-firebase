6e1830b5553d001167eed7edc8623ab5c2014139
Edvin Larsson <el223na@student.lnu.se>
2019-11-13 17:51:55 +0100

    Displays text strings that gets reversed from cloud (#10)
    * gets contents in good way
    
    * displays test string
    
    * works w. any
    
    * tests w invalid arr, does not break
    
    * rm test thiing
    
    * solves it with interface
    
    * better styling

--- src/components/Reverse.tsx
+++ src/components/Reverse.tsx
@@ -2,6 +2,10 @@ import React, { FunctionComponent } from 'react';
 import ReverseForm from './ReverseForm';
 import firebase from 'firebase/app';
 import 'firebase/database';
+import { useListVals } from 'react-firebase-hooks/database';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
 
 type ReverseProps = {
   user: firebase.User;
@@ -10,21 +14,45 @@ type ReverseProps = {
 const Reverse: FunctionComponent<ReverseProps> = ({
   user
 }) => {
+  const dbTextsPath = `users/${user.uid}/texts`;
+  const dbRef = firebase.database().ref(dbTextsPath);
+
+  interface TextObject {
+    content: string;
+  }
+
+  const [texts] = useListVals<TextObject>(dbRef);
+
   return (
-    <>
-      <div
-        style={{ textAlign: 'center', marginTop: '33px' }}
+    <div
+      style={{
+        marginTop: '33px',
+        marginLeft: '15px'
+      }}
+    >
+      <ReverseForm
+        onSubmit={({ text }) => {
+          dbRef.push({ content: text });
+        }}
+      />
+
+      <Typography
+        variant="h5"
+        style={{ marginTop: '33px' }}
       >
-        <ReverseForm
-          onSubmit={({ text }) => {
-            firebase
-              .database()
-              .ref(`users/${user.uid}/texts`)
-              .push({ content: text });
-          }}
-        />
-      </div>
-    </>
+        Reversed text strings:
+      </Typography>
+      <List>
+        {texts &&
+          texts.reverse().map((text, index) => (
+            <ListItem key={index}>
+              <Typography key={index}>
+                {text.content}
+              </Typography>
+            </ListItem>
+          ))}
+      </List>
+    </div>
   );
 };
 

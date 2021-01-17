4d7a5eb3b3693cf28b076fd675eb4430baa459b3
edvinpontuslarsson <el223na@student.lnu.se>
2021-01-17 14:20:22 +0100

    pending header client

--- src/components/Reverse.tsx
+++ src/components/Reverse.tsx
@@ -19,9 +19,21 @@ const Reverse: FunctionComponent<ReverseProps> = ({
 
   interface TextObject {
     content: string;
+    isPending: boolean;
   }
 
   const [texts] = useListVals<TextObject>(dbRef);
+
+  const pending: TextObject[] = [];
+  const reversed: TextObject[] = [];
+
+  if (texts) {
+    for (let i = 0; i < texts.length; i++) {
+      texts[i].isPending
+        ? pending.push(texts[i])
+        : reversed.push(texts[i]);
+    }
+  }
 
   return (
     <div
@@ -32,26 +44,51 @@ const Reverse: FunctionComponent<ReverseProps> = ({
     >
       <ReverseForm
         onSubmit={({ text }) => {
-          dbRef.push({ content: text });
+          dbRef.push({ content: text, isPending: true });
         }}
       />
 
-      <Typography
-        variant="h5"
-        style={{ marginTop: '33px' }}
-      >
-        Reversed text strings:
-      </Typography>
-      <List>
-        {texts &&
-          [...texts].reverse().map((text, index) => (
-            <ListItem key={index}>
-              <Typography key={index}>
-                {text.content}
-              </Typography>
-            </ListItem>
-          ))}
-      </List>
+      {/** TODO can have a common function for both */}
+
+      {pending.length !== 0 && (
+        <>
+          <Typography
+            variant="h5"
+            style={{ marginTop: '33px' }}
+          >
+            Pending text strings:
+          </Typography>
+          <List>
+            {[...pending].reverse().map((text, index) => (
+              <ListItem key={index}>
+                <Typography key={index}>
+                  {text.content}
+                </Typography>
+              </ListItem>
+            ))}
+          </List>
+        </>
+      )}
+
+      {reversed.length !== 0 && (
+        <>
+          <Typography
+            variant="h5"
+            style={{ marginTop: '33px' }}
+          >
+            Reversed text strings:
+          </Typography>
+          <List>
+            {[...reversed].reverse().map((text, index) => (
+              <ListItem key={index}>
+                <Typography key={index}>
+                  {text.content}
+                </Typography>
+              </ListItem>
+            ))}
+          </List>
+        </>
+      )}
     </div>
   );
 };
